cb22c2d4c412c8ab9ec041d33c53a5d7b4296b40
sne-5249 <sne.5249.k@gmail.com>
2020-12-04 09:09:51 +0900

    ユーザ情報作成のルーティングを設定

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	//MethodsメソッドでHTTPメソッドの種類を指定
 	r.HandleFunc("/health", health.HealthCheck).Methods("GET")
 	r.HandleFunc("/user", user.GetAllUser).Methods("GET")
+	r.HandleFunc("/user/create", user.CreateUser).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
