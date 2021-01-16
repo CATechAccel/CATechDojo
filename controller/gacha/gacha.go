ae564faa58137cf3e52433517b45171116944e8e
sne-5249 <sne.5249.k@gmail.com>
2021-01-17 01:37:19 +0900

    ランダムにキャラが当選するガチャを作成

--- controller/gacha/gacha.go
+++ controller/gacha/gacha.go
@@ -7,10 +7,12 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
-func Draw(w http.ResponseWriter, r *http.Request) {
+func DrawSpecificCharacter(w http.ResponseWriter, r *http.Request) {
 	//x-tokenを受け取る
 	token := r.Header.Get("x-token")
 	if token == "" {
@@ -19,7 +21,45 @@ func Draw(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//timesを受け取る
+	//特定のキャラクターをDBから取り出す
+	c := gacha.New()
+
+	if err := c.SelectCharacter(); err != nil {
+		log.Println(err)
+		http.Error(w, "データを参照できませんでした", http.StatusInternalServerError)
+	}
+
+	//取り出したキャラクターをDBに保存する
+	userCharacterID, err := user.CreateUUID()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	if err := c.InsertCharacter(userCharacterID, token); err != nil {
+		log.Println(err)
+		http.Error(w, "ユーザデータを保存できませんでした", http.StatusInternalServerError)
+	}
+
+	//取り出したキャラクターのidとnameをjson形式で返す
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	data, err := json.Marshal(c)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	_, _ = w.Write(data)
+}
+
+func Draw(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("x-token")
+	if token == "" {
+		log.Println("トークンの値がnilです")
+		http.Error(w, "認証情報が必要です。", http.StatusBadRequest)
+		return
+	}
+
 	body := r.Body
 	defer body.Close()
 
@@ -35,33 +75,64 @@ func Draw(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	//特定のキャラクターをDBから取り出す
-	c := gacha.New()
+	//gacha_oddsテーブルから全件取得
+	g := gacha.New()
 
-	if err := c.Select(); err != nil {
-		log.Println(err)
+	odds, err := g.SelectAllOdds()
+	if err != nil {
 		http.Error(w, "データを参照できませんでした", http.StatusInternalServerError)
 	}
 
-	//取り出したキャラクターをDBに保存する
+	//乱数を作成
+	rand.Seed(time.Now().UnixNano())
+	randam := rand.Intn(oddsSum(odds))
+
+	//カウントアップ用変数を定義
+	var count int
+	var hitCharacterID *string
+
+	//当選キャラクターの決定
+	for i, _ := range odds {
+		count += odds[i].Odds
+
+		if count < randam {
+			continue
+		}
+		h := odds[i].CharacterID
+		hitCharacterID = &h
+		break
+	}
+
+	if err := g.SelectHitCharacter(*hitCharacterID); err != nil {
+		log.Println()
+		http.Error(w, "データを参照できませんでした", http.StatusInternalServerError)
+	}
+
 	userCharacterID, err := user.CreateUUID()
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
-	if err := c.Insert(userCharacterID, token); err != nil {
+	if err := g.InsertHitCharacter(token, userCharacterID, *hitCharacterID); err != nil {
 		log.Println(err)
 		http.Error(w, "ユーザデータを保存できませんでした", http.StatusInternalServerError)
 	}
 
-	//取り出したキャラクターのidとnameをjson形式で返す
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	data, err := json.Marshal(c)
+	data, err := json.Marshal(g)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	_, _ = w.Write(data)
+}
+
+func oddsSum(odds []gacha.GachaData) int {
+	var sum int
+	for _, gachadata := range odds {
+		sum += gachadata.Odds
+	}
+	return sum
 }
