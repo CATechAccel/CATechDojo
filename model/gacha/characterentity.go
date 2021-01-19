3b2925fc9ba6ca937e0287325b7db5833edf5f3f
sne-5249 <sne.5249.k@gmail.com>
2021-01-19 20:37:44 +0900

    構造体名を修正

--- model/gacha/characterentity.go
+++ model/gacha/characterentity.go
@@ -1,6 +1,6 @@
 package gacha
 
-type GachaData struct {
+type CharacterData struct {
 	CharacterID string `json:"character_id"`
 	Name        string `json:"name"`
 	Odds        int    `json:"odds"`
