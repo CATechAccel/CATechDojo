3f2d34055abae8616f1b591f4d828761d68a3d9c
sne-5249 <sne.5249.k@gmail.com>
2021-01-21 00:17:44 +0900

    コントローラとモデルの記述について、元の記述をコメントアウトし、インターフェースを用いた実装を追記

--- controller/character/character.go
+++ controller/character/character.go
@@ -4,10 +4,62 @@ import (
 	"CATechDojo/controller/response"
 	"CATechDojo/model/character"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
+
+func ShowUserCharacters(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("x-token")
+	if token == "" {
+		log.Println("トークンの値がnilです")
+		http.Error(w, "認証情報が必要です。", http.StatusBadRequest)
+		return
+	}
+
+	c := character.New()
+
+	//user_idの取得
+	userID, err := c.SelectUserID(token)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "データを参照できませんでした", http.StatusInternalServerError)
+	}
+
+	//user_idを用いてuser_character_id, character_idを取得
+	userCharacters, err := c.SelectUserCharacters(userID)
+	if err != nil {
+		http.Error(w, "データを参照できませんでした", http.StatusInternalServerError)
+	}
+
+	var userCharacterSlice []character.UserCharacterData
+	for _, u := range userCharacters {
+		characterName, err := c.SelectCharacterName(u.CharacterID)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "データを参照できませんでした", http.StatusInternalServerError)
+		}
+		res := character.UserCharacterData{
+			UserCharacterID: u.UserCharacterID,
+			CharacterID:     u.CharacterID,
+			Name:            characterName,
+		}
+		userCharacterSlice = append(userCharacterSlice, res)
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	res := response.CharactersResponse{userCharacterSlice}
+	data, err := json.Marshal(res)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	_, _ = w.Write(data)
+
+}
+
+/*
 
 func ShowUserCharacters(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("x-token")
@@ -62,3 +114,5 @@ func getCharacterName(characterID string) string {
 	}
 	return characterName
 }
+
+*/
--- model/character/character.go
+++ model/character/character.go
@@ -2,6 +2,52 @@ package character
 
 import "CATechDojo/db"
 
+type userCharacterInterface interface {
+	SelectUserID(token string) (string, error)
+	SelectUserCharacters(UserID string) ([]UserCharacterData, error)
+	SelectCharacterName(CharacterID string) (string, error)
+}
+
+func New() userCharacterInterface {
+	return &UserCharacterData{}
+}
+
+func (c *UserCharacterData) SelectUserID(token string) (string, error) {
+	var userID string
+	row := db.DBInstance.QueryRow("SELECT user_id FROM users WHERE auth_token = ?", token)
+	if err := row.Scan(&userID); err != nil {
+		return "", err
+	}
+	return userID, nil
+}
+
+func (c *UserCharacterData) SelectUserCharacters(UserID string) ([]UserCharacterData, error) {
+	rows, err := db.DBInstance.Query("SELECT user_character_id, character_id FROM user_characters WHERE user_id =?", UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	userCharacterSlice := make([]UserCharacterData, 0)
+	for rows.Next() {
+		var u UserCharacterData
+		if err := rows.Scan(&u.UserCharacterID, &u.CharacterID); err != nil {
+			return nil, err
+		}
+		userCharacterSlice = append(userCharacterSlice, u)
+	}
+	return userCharacterSlice, nil
+}
+
+func (c *UserCharacterData) SelectCharacterName(CharacterID string) (string, error) {
+	row := db.DBInstance.QueryRow("SELECT name FROM characters WHERE id = ?", CharacterID)
+	if err := row.Scan(&c.Name); err != nil {
+		return "", err
+	}
+	return c.Name, nil
+}
+
+/*
+
 func SelectUserID(token string) (string, error) {
 	var userID string
 	row := db.DBInstance.QueryRow("SELECT user_id FROM users WHERE auth_token = ?", token)
@@ -36,3 +82,5 @@ func SelectCharacterName(CharacterID string) (string, error) {
 	}
 	return c.Name, nil
 }
+
+*/
