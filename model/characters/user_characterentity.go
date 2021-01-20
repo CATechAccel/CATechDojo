b6bfbfed532cefa3841a2eef2a10201f47ca7e85
sne-5249 <sne.5249.k@gmail.com>
2021-01-20 14:26:06 +0900

    ユーザーが所持するキャラクター情報の構造体を定義

---
+++ model/characters/user_characterentity.go
@@ -0,0 +1,7 @@
+package characters
+
+type UserCharacterData struct {
+	UserCharacterID string `json:"user_character_id"`
+	CharacterID     string `json:"character_id"`
+	Name            string `json:"name"`
+}
